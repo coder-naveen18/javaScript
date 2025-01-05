3469c188dae4da69e4f9231260c7d77f444c1158
NAVEEN SAHU <119333274+coder-naveen18@users.noreply.github.com>
2025-01-05 16:07:32 +0000

    variable config done

---
+++ 02_variable/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "naveensahu983@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+
+
+
+// analysing
+
+// accountId =2
+console.log(accountId);
+
+
