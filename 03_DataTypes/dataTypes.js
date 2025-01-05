0d53e8a876639d03f535943baa2bca325e863046
NAVEEN SAHU <119333274+coder-naveen18@users.noreply.github.com>
2025-01-05 17:07:29 +0000

    datatypes config done

---
+++ 03_DataTypes/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+
+// alert(3 + 3)   // we are using node js, not browser
+
+
+let name = "Naveen"  //string
+let age = 32  // number  ==> 2 to power 53  (range)
+// bigint
+let isLoggedIn = false // boolean
+let state = null //null --> it's a stand alone value
+//undefined  --> we have created a variable but did not assigned the value to it.
+//symbol --> for finding the uniqueness
+
+
+
+// object
+
+
+console.log(typeof "");
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
+
