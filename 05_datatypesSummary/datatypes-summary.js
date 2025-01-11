e7963c849f2990fd262eab0b7e2ace5284c5eb30
NAVEEN SAHU <119333274+coder-naveen18@users.noreply.github.com>
2025-01-11 09:13:08 +0000

    updation done

---
+++ 05_datatypesSummary/datatypes-summary.js
@@ -0,0 +1,44 @@
+// JS ---> is a dynamically typed language
+
+//  Primitive ----> Call by value
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+
+// Array
+const heros = ["shaktiman", "naagraj", "doga"];
+
+// Objects
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+
+// Functions
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
