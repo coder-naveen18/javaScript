eb7e76433c9836d4bcb2bcfb44e5f4824d09d976
NAVEEN SAHU <119333274+coder-naveen18@users.noreply.github.com>
2025-01-11 07:25:24 +0000

    conversion understand

---
+++ 04_ConversionOperation/conversion.js
@@ -0,0 +1,42 @@
+let score = "100";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let value = "55abc";
+
+
+let valueInNumber = Number(value);
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+
+// when we convert a number "100" => 100
+// "33abc" => NaN
+// true => 1; false => 0;
+
+
+let isLoggedIn = "Naveen";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true ; 0 => false ;
+// "" => false
+// "String" => true;
+
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(someNumber);
+console.log(typeof stringNumber);
+
+
+
+
