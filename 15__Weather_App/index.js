9e965d793d7a87019c8f726de15cf31aa2f7fb7e
NAVEEN SAHU <119333274+coder-naveen18@users.noreply.github.com>
2025-01-23 17:44:17 +0000

    Done

--- 15__Weather_App/index.js
+++ 15__Weather_App/index.js
@@ -5,29 +5,52 @@ let searchButton = document.getElementById('button');
 let Result = document.querySelector('.result');
 
 
-// let getWeather = ()=>{
 
-//     let cityValue = CityName.value;
-//     console.log(cityValue)
 
-//     if(cityValue.length === 0){
-//         Result.innerHTML= `<h3> Please enter a valid City Name </h3>`;
-//     }
-//     else{
-    
-//         let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q={cityValue}&appid={79eea62ac27bae462ecf01bc252bacbf}&units=metric';
-    
-//         fetch(requestUrl).then((res)=> res.json()).then((data)=>{
-//             console.log(data)
-//             console.log(data.weather[0])
-//         })
+let getWeather = ()=>{
+
+    let cityValue = CityName.value;
+
+    if(cityValue.length == 0){
+        Result.innerHTML= `<h3> Please enter a valid City Name </h3>`;
+    }
+    else{
+
+    let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=79eea62ac27bae462ecf01bc252bacbf&units=metric`;
+
+        fetch(requestUrl).then((resp)=> resp.json()).then((data)=>{
+
+            Result.innerHTML = `
+         <div class="Result_Of_Data">
+            <h1>${data.name}</h1>
+            <h2>${data.weather[0].description}</h2>
+            <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" />
+            <h1 id="Main_temp">${data.main.temp} &#176; </h1>
+        <div class="temp_ratios">
+            <div class="MinTemp">
+            <p>Min Temp</p>
+            <p>${data.main.temp_min} &#176;</p>
+            </div>
+            <div >
+            <p>Max Temp </p>
+            <p>${data.main.temp_max} &#176;</p></div>
+            </div>
+            </div>`
+
+           
+        })
         
-//     }
+        .catch(()=>{
+            Result.innerHTML = `<p> City not found </p>`
+    })
 
-    
-// }
+    }   
+}
 
-getWeather();
+
+
+searchButton.addEventListener("click", getWeather);
+window.addEventListener("load" , getWeather);
 
 
 
